85e575f154c42813a3121a5bda8b467163df9135
WalterBryant <490560753@qq.com>
2018-10-24 13:21:39 +0800

    完全由Paint 出来的 Minion

---
+++ app/src/main/java/com/w/proandroid/MinionActivity.java
@@ -0,0 +1,17 @@
+package com.w.proandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Paint 小黄人
+ */
+
+public class MinionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minion);
+    }
+}

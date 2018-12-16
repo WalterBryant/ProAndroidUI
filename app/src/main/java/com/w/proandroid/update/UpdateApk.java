dab412a20db276ad025750fbcb22d1c14ccc9e00
WalterBryant <walterbryant@163.com>
2018-12-17 00:02:39 +0800

    update update

---
+++ app/src/main/java/com/w/proandroid/update/UpdateApk.java
@@ -0,0 +1,21 @@
+package com.w.proandroid.update;
+
+
+public class UpdateApk {
+
+    /**
+     * 下载地址
+     */
+    public String downurl;
+    /**
+     * 更新内容
+     */
+    public String desc;
+    /**
+     * 强制更新
+     */
+    public int force;
+
+    public int digstv;
+
+}
